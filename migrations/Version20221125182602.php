2e302a46a82804cb3a1daea1f8dae5ef8d5979f4
msajjad650 <muhammadsajaad8@gmail.com>
2022-11-25 23:37:04 +0100

    update db migration

--- migrations/Version20221125182602.php
+++ migrations/Version20221125182602.php
@@ -20,7 +20,7 @@ final class Version20221125182602 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE ticket (id INT AUTO_INCREMENT NOT NULL, ticket_id VARCHAR(20) DEFAULT NULL, source VARCHAR(50) DEFAULT NULL, destination VARCHAR(50) DEFAULT NULL, departure_time VARCHAR(50) DEFAULT NULL, seat_number INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE ticket (id INT AUTO_INCREMENT NOT NULL, passport_id VARCHAR(20) DEFAULT NULL, source VARCHAR(50) DEFAULT NULL, destination VARCHAR(50) DEFAULT NULL, departure_time VARCHAR(50) DEFAULT NULL, seat_number INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
